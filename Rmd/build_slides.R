9895d42fc6f4ff5b40fd89b86835b16446389859
albertotb <albertotb@gmail.com>
2019-04-01 17:46:16 +0200

    improve build_slides

--- Rmd/build_slides.R
+++ Rmd/build_slides.R
@@ -1,6 +1,12 @@
 library(pagedown)
 library(tools)
 
+input_dir  <- "./Rmd"
+output_dir <- "./slides"
+
+# 1. Take all .Rmd files from `input_dir`
+# 2. Convert them to PDF using `chrome_print()`
+# 3. Move them to `output_dir`
 for (file in list.files(path = "./Rmd", pattern = ".Rmd", recursive = TRUE)) {
   input <- file.path("./Rmd", file)
   output <- paste0(file_path_sans_ext(input), ".pdf")
