8eb8fb4e4a4c03c3381e70f9fe1a6b049bba1e6a
albertotb <albertotb@gmail.com>
2019-04-08 03:52:51 +0200

    general structure

--- Rmd/04-regression/macros.js
+++ Rmd/04-regression/macros.js
@@ -3,8 +3,14 @@ remark.macros.scale = function (percentage) {
   return '<img src="' + url + '" style="width: ' + percentage + '" />';
 };
 
+remark.macros.colorText = function(color) {
+  var text = this;
+  return `<span style="color:${color}">${text}</span>`;
+  //return `<span class="${color}">${text}</span>`;
+};
+
 remark.macros.vspace = function (percentage) {
-  return `<div style="height: ${percentage}%"></div>`
+  return `<div style="height: ${percentage}%"></div>`;
 };
 
 remark.macros.centerScale = function (percentage) {
@@ -12,4 +18,11 @@ remark.macros.centerScale = function (percentage) {
   return `<div style="text-align:center;">
             <img src='${url}' style=width: ${percentage}%/>
           </div>`;
+};
+
+remark.macros.video = function(width, height) {
+  var url = this;
+  return `<video width="${width}" height="${height}">
+            <source src="${url}" type="video/mp4">
+          </video>`;
 };
