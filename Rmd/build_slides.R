02127f58dbdae6ab2404367f4e0088c199b49b7c
albertotb <albertotb@gmail.com>
2019-04-10 20:06:00 +0200

    Añadir PDF

--- Rmd/build_slides.R
+++ Rmd/build_slides.R
@@ -7,10 +7,10 @@ output_dir <- "./slides"
 # 1. Take all .Rmd files from `input_dir`
 # 2. Convert them to PDF using `chrome_print()`
 # 3. Move them to `output_dir`
-for (file in list.files(path = "./Rmd", pattern = ".Rmd", recursive = TRUE)) {
-  input <- file.path("./Rmd", file)
+for (file in list.files(path = input_dir, pattern = ".Rmd", recursive = TRUE)) {
+  input <- file.path(input_dir, file)
   output <- paste0(file_path_sans_ext(input), ".pdf")
   pagedown::chrome_print(input)
-  file.copy(output, "./slides")
+  file.copy(output, output_dir)
   file.remove(output)
 }
